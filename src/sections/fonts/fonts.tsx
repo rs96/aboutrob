04923b12ff1e5795530daa98e164046f3d83da4e
Robert Shipley <shipleymrobert@gmail.com>
2023-01-23 16:33:28 +0000

    bit of work on fonts

--- src/sections/fonts/fonts.tsx
+++ src/sections/fonts/fonts.tsx
@@ -3,9 +3,22 @@ import { isMobile } from "../../queries";
 import { FontCard } from "./fontCard";
 
 export const Fonts = () => (
-  <div className={`font-page card-area ${isMobile() && "mobile"}`}>
-    {["Test", "Example", "This One", "And Another"].map((font) => (
-      <FontCard name={font} />
-    ))}
-  </div>
+    <div className={`font-page card-area ${isMobile() && "mobile"}`}>
+        {[
+            "Helvetica",
+            "Verdana",
+            "Comic Sans MS",
+            "Times New Roman",
+            "Arial",
+            "Arial Black",
+            "Tahoma",
+            "Trebuchet MS",
+            "Impact",
+            "Gill Sans",
+            "Georgia",
+            "Palatino",
+        ].map((font) => (
+            <FontCard name={font} key={font} />
+        ))}
+    </div>
 );
